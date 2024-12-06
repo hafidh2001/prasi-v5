e36584dbb8be0242ce881b89a1408b3b0ba96fb1
HAFIDH AHMAD FAUZAN <hafidhfauzan2@gmail.com>
2024-12-06 11:19:02 +0700

    feat: inspect oracle

---
+++ backend/srv/utils/query/connectors/oracle/utils/get-all.ts
@@ -0,0 +1,21 @@
+import type { OracleConfig } from "../internal";
+
+export const oracleGetAll = async <T>(c: OracleConfig, sql: string) => {
+  const data = await c.conn?.execute<string>(sql);
+  const result = [] as T[];
+
+  for (const row of data?.rows || []) {
+    const item = {} as any;
+
+    for (let i = 0; i < Object.keys(row).length; i++) {
+      const meta = (data?.metaData || [])[i];
+
+      if (meta) {
+        item[meta.name] = row[i];
+      }
+    }
+    result.push(item);
+  }
+
+  return result;
+};
