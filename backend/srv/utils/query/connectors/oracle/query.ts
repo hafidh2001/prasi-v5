2c303b1bf3e1f8ac0b4d32d3f8c497333447a2e3
HAFIDH AHMAD FAUZAN <hafidhfauzan2@gmail.com>
2024-12-16 14:26:46 +0700

    feat: query getJoin

--- backend/srv/utils/query/connectors/oracle/query.ts
+++ backend/srv/utils/query/connectors/oracle/query.ts
@@ -1,6 +1,7 @@
 import type { PQuerySelect } from "prasi-frontend/src/nova/ed/mode/query/types";
 import type { QInspectResult } from "utils/query/types";
 import { getColumns } from "./utils/get-columns";
+import { getJoins } from "./utils/get-joins";
 
 export const query = async (
   inspected_scheme: QInspectResult,
@@ -8,5 +9,8 @@ export const query = async (
 ): Promise<any> => {
   const { select, table } = pq;
   const x = getColumns(inspected_scheme, table, select);
-  console.log({x});
+  console.log({ x });
+
+  const y = getJoins(inspected_scheme, table, select);
+  console.log({ y });
 };
