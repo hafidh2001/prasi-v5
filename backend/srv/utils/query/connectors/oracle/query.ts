407f01848872455df4f410c88e3520676ccf13e9
HAFIDH AHMAD FAUZAN <hafidhfauzan2@gmail.com>
2024-12-18 11:11:17 +0700

    feat: query getOrderBy

--- backend/srv/utils/query/connectors/oracle/query.ts
+++ backend/srv/utils/query/connectors/oracle/query.ts
@@ -3,6 +3,7 @@ import type { QInspectResult } from "utils/query/types";
 import { getColumns } from "./utils/get-columns";
 import { getJoins } from "./utils/get-joins";
 import { getWheres } from "./utils/get-wheres";
+import { getOrdersBy } from "./utils/get-orders-by";
 
 export const query = async (
   inspected_scheme: QInspectResult,
@@ -11,15 +12,18 @@ export const query = async (
   columns_arr: string[];
   joins_arr: string[];
   wheres_arr: string[];
+  orders_by_arr: string[];
 }> => {
-  const { select, table, where } = pq;
+  const { select, table, where, order_by } = pq;
   const columns_arr = getColumns(inspected_scheme, table, select);
   const joins_arr = getJoins(inspected_scheme, table, select);
-  const wheres_arr = getWheres(inspected_scheme, table, select, where);
+  const wheres_arr = getWheres(table, select, where);
+  const orders_by_arr = getOrdersBy(table, select, order_by);
 
   return {
     columns_arr,
     joins_arr,
     wheres_arr,
+    orders_by_arr,
   };
 };
